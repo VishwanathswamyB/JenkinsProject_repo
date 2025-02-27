f11b9a0a1e109d0469c147522cea5881131d983d
Computer <Computer@DESKTOP-S1II299>
2025-02-27 19:22:46 +0530

    first jenkins

---
+++ JernkinsProject/src/test/java/Jenkins/DemoClassTest.java
@@ -0,0 +1,14 @@
+package Jenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoClassTest {
+	@Test
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
