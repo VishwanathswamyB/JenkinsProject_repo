2523a716423b29efa4319d63723de819ad5eeeda
Computer <Computer@DESKTOP-S1II299>
2025-02-27 20:00:03 +0530

    upddate

--- JernkinsProject/src/test/java/Jenkins/DemoClassTest.java
+++ JernkinsProject/src/test/java/Jenkins/DemoClassTest.java
@@ -2,12 +2,14 @@ package Jenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class DemoClassTest {
 	@Test
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
+		Reporter.log("first script for jenkins",true);
 		
 	}
 
